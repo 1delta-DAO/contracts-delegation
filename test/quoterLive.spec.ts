41d62692e2d5e16082627de661bc65111a9eeef9
Achthar <69119535+Achthar@users.noreply.github.com>
2023-10-22 21:25:44 +0200

    [fix] use consistent test input

--- test/quoterLive.spec.ts
+++ test/quoterLive.spec.ts
@@ -60,7 +60,7 @@ it("Test custom multicall", async function () {
         [0]
     )
     const callData = quoter.interface.encodeFunctionData('quoteExactInput', [
-        '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619000064002791bca1f2de4661ed88a30c99a7a9449aa84174',
+        pathDelta,
         '0x22b1c8c1227a0000'
     ])
 
