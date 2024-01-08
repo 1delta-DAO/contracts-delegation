a0eac67dab8f0436c5bc899c9e6375a4adc25f65
Achthar <69119535+Achthar@users.noreply.github.com>
2024-01-09 00:44:23 +0100

    rename vars

--- contracts/1delta/proxy/DeltaBroker.sol
+++ contracts/1delta/proxy/DeltaBroker.sol
@@ -27,19 +27,22 @@ contract DeltaBrokerProxy {
         LibModules.configureModules(cut);
     }
 
+    // An efficient multicall implementation for directly calling functions across multiple modules
+    // Note that this multicall is payable, as such, multi-spending has to be taken into account by 
+    // the caller
     function multicall(bytes[] calldata data) external payable {
         mapping(bytes4 => address) storage impls = LibModules.moduleStorage().selectorToModule;
         assembly {
             mstore(0x00, 0x20)
-            let results := 0x40
+            let tracker := 0x40
             // `shl` 5 is equivalent to multiplying by 0x20.
             let end := shl(5, data.length)
             // Copy the offsets from calldata into memory.
             calldatacopy(0x40, data.offset, end)
-            // Offset into `results`.
-            let resultsOffset := end
-            // Pointer to the end of `results`.
-            end := add(results, end)
+            // Offset into `tracker`.
+            let currentOffset := end
+            // Pointer to the end of `tracker`.
+            end := add(tracker, end)
 
             for {
 
@@ -47,8 +50,8 @@ contract DeltaBrokerProxy {
 
             } {
                 // The offset of the current bytes in the calldata.
-                let o := add(data.offset, mload(results))
-                let m := add(resultsOffset, 0x40)
+                let o := add(data.offset, mload(tracker))
+                let m := add(currentOffset, 0x40)
                 // Copy the current bytes from calldata to the memory.
                 calldatacopy(
                     m,
@@ -60,32 +63,24 @@ contract DeltaBrokerProxy {
                     mload(m), // calldata was copied to 0, we load the selector from there
                     0xFFFFFFFF00000000000000000000000000000000000000000000000000000000
                 )
-                mstore(0, target)
+                mstore(0x00, target)
                 mstore(0x20, impls.slot)
-                let slot := keccak256(0, 0x40)
+                let slot := keccak256(0x00, 0x40)
                 // assign module
                 target := sload(slot)
                 if iszero(target) {
                     // Reverting with NoImplementation
-                    mstore(0, 0x6826a5a500000000000000000000000000000000000000000000000000000000)
-                    revert(0, 4)
+                    mstore(0x00, 0x6826a5a500000000000000000000000000000000000000000000000000000000)
+                    revert(0x00, 4)
                 }
-
+                // execute the current call
                 if iszero(delegatecall(gas(), target, m, calldataload(o), codesize(), 0x00)) {
                     // Bubble up the revert if the delegatecall reverts.
                     returndatacopy(0x00, 0x00, returndatasize())
                     revert(0x00, returndatasize())
                 }
-                // Append the current `resultsOffset` into `results`.
-                mstore(results, resultsOffset)
-                results := add(results, 0x20)
-                // Append the `returndatasize()`, and the return data.
-                mstore(m, returndatasize())
-                returndatacopy(add(m, 0x20), 0x00, returndatasize())
-                // Advance the `resultsOffset` by `returndatasize() + 0x20`,
-                // rounded up to the next multiple of 32.
-                resultsOffset := and(add(add(resultsOffset, returndatasize()), 0x3f), 0xffffffffffffffe0)
-                if iszero(lt(results, end)) {
+                tracker := add(tracker, 0x20)
+                if iszero(lt(tracker, end)) {
                     break
                 }
             }
@@ -101,29 +96,29 @@ contract DeltaBrokerProxy {
         assembly {
             let cdlen := calldatasize()
             // Store at 0x40, to leave 0x00-0x3F for slot calculation below.
-            calldatacopy(0x40, 0, cdlen)
+            calldatacopy(0x40, 0x00, cdlen)
             let target := and(mload(0x40), 0xFFFFFFFF00000000000000000000000000000000000000000000000000000000)
 
             // Slot for impls[target] is keccak256(target . impls.slot).
-            mstore(0, target)
+            mstore(0x00, target)
             mstore(0x20, impls.slot)
-            let slot := keccak256(0, 0x40)
+            let slot := keccak256(0x00, 0x40)
             target := sload(slot) // overwrite target to delegate address
             if iszero(target) {
                 // Revert with:
                 // abi.encodeWithSelector(
                 //   bytes4(keccak256("NoImplementation()")))
-                mstore(0, 0x6826a5a500000000000000000000000000000000000000000000000000000000)
-                revert(0, 4)
+                mstore(0x00, 0x6826a5a500000000000000000000000000000000000000000000000000000000)
+                revert(0x00, 4)
             }
 
-            let success := delegatecall(gas(), target, 0x40, cdlen, 0, 0)
+            let success := delegatecall(gas(), target, 0x40, cdlen, 0x00, 0x00)
             let rdlen := returndatasize()
-            returndatacopy(0, 0, rdlen)
+            returndatacopy(0x00, 0x00, rdlen)
             if success {
-                return(0, rdlen)
+                return(0x00, rdlen)
             }
-            revert(0, rdlen)
+            revert(0x00, rdlen)
         }
     }
 
