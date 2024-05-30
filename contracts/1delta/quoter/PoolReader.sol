f1e813032478138bab83aeade5a73948899c7832
caglavol <18139560+caglavol@users.noreply.github.com>
2024-05-30 14:09:00 +0300

    add indexTo param and try catch to external calls

--- contracts/1delta/quoter/PoolReader.sol
+++ contracts/1delta/quoter/PoolReader.sol
@@ -28,7 +28,7 @@ contract PoolReader {
         uint256 totalSupply;
         uint256 reserve;
     }
-    
+
     struct UniswapV2Info {
         uint256 index;
         address pairAddress;
@@ -36,50 +36,98 @@ contract PoolReader {
         ERC20Info token1Info;
     }
 
-    function getUniswapV2Info(address factoryAddress, uint256 indexFrom) external view returns (UniswapV2Info[] memory) {
+    function getUniswapV2Info(
+                address factoryAddress, 
+                uint256 indexFrom, 
+                uint256 indexTo
+                ) external view returns (UniswapV2Info[] memory) {
+
         IFactory factory = IFactory(factoryAddress);
         uint256 totalPools = factory.allPairsLength();
-        
-        require(indexFrom < totalPools, "Index out of range");
-        
-        UniswapV2Info[] memory poolsInfo = new UniswapV2Info[](totalPools - indexFrom);
-        
-        for (uint256 i = indexFrom; i < totalPools; i++) {
+
+        if (indexTo > totalPools) {
+            indexTo = totalPools;
+        }
+
+        require(indexFrom < indexTo, "Index out of range");
+
+        UniswapV2Info[] memory tempPoolsInfo = new UniswapV2Info[](indexTo - indexFrom);
+
+        uint poolIndex = 0;
+        for (uint256 i = indexFrom; i < indexTo; i++) {
             address poolAddress = factory.allPairs(i);
             IPool pool = IPool(poolAddress);
 
-            address token0Address = pool.token0();
-            address token1Address = pool.token1();
-            
-            IERC20 token0 = IERC20(token0Address);
-            IERC20 token1 = IERC20(token1Address);
-            
-            ERC20Info memory token0Info = ERC20Info({
-                tokenAddress: token0Address,
-                name: token0.name(),
-                symbol: token0.symbol(),
-                decimals: token0.decimals(),
-                totalSupply: token0.totalSupply(),
-                reserve: token0.balanceOf(poolAddress)
-            });
-            
-            ERC20Info memory token1Info = ERC20Info({
-                tokenAddress: token1Address,
-                name: token1.name(),
-                symbol: token1.symbol(),
-                decimals: token1.decimals(),
-                totalSupply: token1.totalSupply(),
-                reserve: token1.balanceOf(poolAddress)
-            });
-            
-            poolsInfo[i] = UniswapV2Info({
-                index: i,
-                pairAddress: poolAddress,
-                token0Info: token0Info,
-                token1Info: token1Info
-            });
+            address token0Address;
+            address token1Address;
+
+            try pool.token0() returns (address addr) {
+                token0Address = addr;
+            } catch {
+                continue;
+            }
+
+            try pool.token1() returns (address addr) {
+                token1Address = addr;
+            } catch {
+                continue;
+            }
+
+            ERC20Info memory token0Info = getTokenInfo(token0Address, poolAddress);
+            ERC20Info memory token1Info = getTokenInfo(token1Address, poolAddress);
+
+            if (bytes(token0Info.name).length == 0 || bytes(token1Info.name).length == 0) {
+                continue;
+            }
+
+            tempPoolsInfo[poolIndex] = UniswapV2Info({index: i, pairAddress: poolAddress, token0Info: token0Info, token1Info: token1Info});
+            poolIndex++;
         }
-        
+
+        UniswapV2Info[] memory poolsInfo = new UniswapV2Info[](poolIndex);
+        for (uint256 i = 0; i < poolIndex; i++) {
+            poolsInfo[i] = tempPoolsInfo[i];
+        }
+
         return poolsInfo;
+    }
+
+    function getTokenInfo(address tokenAddress, address poolAddress) internal view returns (ERC20Info memory) {
+        IERC20 token = IERC20(tokenAddress);
+
+        ERC20Info memory info;
+        info.tokenAddress = tokenAddress;
+
+        try token.name() returns (string memory name) {
+            info.name = name;
+        } catch {
+            return ERC20Info(address(0), "", "", 0, 0, 0);
+        }
+
+        try token.symbol() returns (string memory symbol) {
+            info.symbol = symbol;
+        } catch {
+            return ERC20Info(address(0), "", "", 0, 0, 0);
+        }
+
+        try token.decimals() returns (uint8 decimals) {
+            info.decimals = decimals;
+        } catch {
+            return ERC20Info(address(0), "", "", 0, 0, 0);
+        }
+
+        try token.totalSupply() returns (uint256 totalSupply) {
+            info.totalSupply = totalSupply;
+        } catch {
+            return ERC20Info(address(0), "", "", 0, 0, 0);
+        }
+
+        try token.balanceOf(poolAddress) returns (uint256 reserve) {
+            info.reserve = reserve;
+        } catch {
+            return ERC20Info(address(0), "", "", 0, 0, 0);
+        }
+
+        return info;
     }
 }
