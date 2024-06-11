ab7c0681599813cac009589a4ed138179fc44f27
Achthar <69119535+Achthar@users.noreply.github.com>
2024-06-11 14:54:58 +0200

    update gas  estimates

--- test/mantle/Composer.t.sol
+++ test/mantle/Composer.t.sol
@@ -716,45 +716,54 @@ contract ComposerTest is DeltaSetup {
     }
 }
 
-// Ran 9 tests for test/mantle/Composer.t.sol:ComposerTest
-// [PASS] test_mantle_composer_borrow() (gas: 916996)
-// Logs:
-//   gas 378726
-//   gas 432641
 
-// [PASS] test_mantle_composer_depo() (gas: 370985)
+// Ran 11 tests for test/mantle/Composer.t.sol:ComposerTest
+// [PASS] test_mantle_composer_borrow() (gas: 917038)
 // Logs:
-//   gas 248953
+//   gas 378730
+//   gas 432645
 
-// [PASS] test_mantle_composer_multi_route_exact_in() (gas: 377195)
+// [PASS] test_mantle_composer_depo() (gas: 371016)
 // Logs:
-//   gas 192081
+//   gas 248957
 
-// [PASS] test_mantle_composer_multi_route_exact_in_native() (gas: 368278)
+// [PASS] test_mantle_composer_multi_route_exact_in() (gas: 377126)
 // Logs:
-//   gas 374362
+//   gas 192085
 
-// [PASS] test_mantle_composer_multi_route_exact_in_native_out() (gas: 633219)
+// [PASS] test_mantle_composer_multi_route_exact_in_native() (gas: 368211)
 // Logs:
-//   gas-exactIn-native-out-2 split 547587
+//   gas 374366
 
-// [PASS] test_mantle_composer_multi_route_exact_in_self() (gas: 399399)
+// [PASS] test_mantle_composer_multi_route_exact_in_native_out() (gas: 633203)
 // Logs:
-//   gas 219226
+//   gas-exactIn-native-out-2 split 547591
 
-// [PASS] test_mantle_composer_multi_route_exact_out() (gas: 390492)
+// [PASS] test_mantle_composer_multi_route_exact_in_self() (gas: 399340)
 // Logs:
-//   gas 190951
+//   gas 219230
 
-// [PASS] test_mantle_composer_repay() (gas: 985698)
+// [PASS] test_mantle_composer_multi_route_exact_out() (gas: 390674)
 // Logs:
-//   gas 378726
-//   gas 432642
-//   gas 102297
+//   gas 190957
 
-// [PASS] test_mantle_composer_withdraw() (gas: 701987)
+// [PASS] test_mantle_composer_multi_route_exact_out_native_in() (gas: 408213)
 // Logs:
-//   gas 378726
-//   gas 253944
+//   gas-exactOut-native-in-2 split 385726
 
-// Suite result: ok. 9 passed; 0 failed; 0 skipped; finished in 147.99ms (22.59ms CPU time)
+// [PASS] test_mantle_composer_multi_route_exact_out_native_out() (gas: 558685)
+// Logs:
+//   gas-exactOut-native-out-2 split 413439
+
+// [PASS] test_mantle_composer_repay() (gas: 985744)
+// Logs:
+//   gas 378730
+//   gas 432646
+//   gas 102301
+
+// [PASS] test_mantle_composer_withdraw() (gas: 702003)
+// Logs:
+//   gas 378730
+//   gas 253948
+
+// Suite result: ok. 11 passed; 0 failed; 0 skipped; finished in 166.70ms (35.96ms CPU time)
