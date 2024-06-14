af5157f78f4ed30eafae365f3fbf6c843aaca365
Achthar <69119535+Achthar@users.noreply.github.com>
2024-06-14 19:36:24 +0200

    new bench, add utils to composer test

--- test/mantle/ComposerUtils.sol
+++ test/mantle/ComposerUtils.sol
@@ -5,14 +5,14 @@ import "../../contracts/1delta/modules/deploy/mantle/composable/Commands.sol";
 
 contract ComposerUtils {
     uint8 DEAULT_MODE = 2;
-    uint256 private constant USE_PERMIT2_FLAG = 1 << 127;
-    // uint256 private constant UNWRAP_NATIVE_MASK = 1 << 254;
-    // uint256 private constant PAY_SELF = 1 << 254;
-    uint256 private constant PAY_SELF = 1 << 255;
-    uint256 private constant UINT128_MASK = 0x00000000000000000000000000000000ffffffffffffffffffffffffffffffff;
-    uint256 private constant UINT112_MASK = 0x000000000000000000000000000000000000ffffffffffffffffffffffffffff;
-    uint256 private constant LENDER_ID_MASK = 0x0000000000000000000000000000000000ff0000000000000000000000000000;
-    uint256 private constant UINT128_MASK_UPPER = 0xffffffffffffffffffffffffffffffff00000000000000000000000000000000;
+    uint256 internal constant USE_PERMIT2_FLAG = 1 << 127;
+    // uint256 internal constant UNWRAP_NATIVE_MASK = 1 << 254;
+    // uint256 internal constant PAY_SELF = 1 << 254;
+    uint256 internal constant PAY_SELF = 1 << 255;
+    uint256 internal constant UINT128_MASK = 0x00000000000000000000000000000000ffffffffffffffffffffffffffffffff;
+    uint256 internal constant UINT112_MASK = 0x000000000000000000000000000000000000ffffffffffffffffffffffffffff;
+    uint256 internal constant LENDER_ID_MASK = 0x0000000000000000000000000000000000ff0000000000000000000000000000;
+    uint256 internal constant UINT128_MASK_UPPER = 0xffffffffffffffffffffffffffffffff00000000000000000000000000000000;
 
     function transferIn(address asset, address receiver, uint256 amount) internal pure returns (bytes memory data) {
         data = abi.encodePacked(
