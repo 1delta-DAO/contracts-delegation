c7440241c534c2f720b48ec4d2af568582b027b5
Achthar <69119535+Achthar@users.noreply.github.com>
2024-07-06 23:25:55 +0200

    new composer address

--- scripts/polygon/addresses/oneDeltaAddresses.ts
+++ scripts/polygon/addresses/oneDeltaAddresses.ts
@@ -3,6 +3,6 @@ export const ONE_DELTA_GEN2_ADDRESSES_POLYGON = {
     proxy: "0x6A6faa54B9238f0F079C8e6CBa08a7b9776C7fE4",
     config: "0xFc7c0Deb7012EF6e930bF681D7C7cF854eC8E528",
     lensImplementation: "0xAC694778b869e2a4c1702C5BADf2B192Cfe83750",
-    composerImplementation: "0xdf686AbC4e4Eb1c3ac287cb4041F3872BC976c94",
+    composerImplementation: "0x627b69E4f1719E19db01F25Be24BD4099df84670",
     managementImplementation: "0x025fD6E2e235329daFf6b29DD6DA7CDD38b22De5"
 }
