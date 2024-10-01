6dc21869774cd6d27d6587686f6d71e2fb3cea0d
caglavol <volkan@1delta.io>
2024-10-01 17:49:26 +0200

    remove setValidTarget call in deployment script

--- scripts/mantle/deployMetaAggWithPermit.ts
+++ scripts/mantle/deployMetaAggWithPermit.ts
@@ -5,11 +5,6 @@ import {
 } from "../../types";
 import { MANTLE_CONFIGS } from "./utils";
 
-const aggregatorsTargets = [
-    '0xD9F4e85489aDCD0bAF0Cd63b4231c6af58c26745', // ODOS
-    '0x6131B5fae19EA4f9D964eAc0408E4408b66337b5' // KYBER
-]
-
 async function main() {
     const accounts = await ethers.getSigners()
     const operator = accounts[0]
@@ -17,15 +12,11 @@ async function main() {
     if (chainId !== 5000) throw new Error("invalid chainId")
     console.log("operator", operator.address, "on", chainId)
 
-    // flash swapper
     const magwp = await new DeltaMetaAggregatorWithPermit__factory(operator).deploy(MANTLE_CONFIGS)
     await magwp.deployed()
     console.log("magwp deployed")
 
     console.log("magwp", magwp.address)
-
-    await magwp.setValidTarget(aggregatorsTargets[0], aggregatorsTargets[0], true, MANTLE_CONFIGS)
-    await magwp.setValidTarget(aggregatorsTargets[1], aggregatorsTargets[1], true, MANTLE_CONFIGS)
 }
 
 main()
