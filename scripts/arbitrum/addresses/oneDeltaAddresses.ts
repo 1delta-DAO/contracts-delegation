afe74bd23684b0f62ddfac26107238c008c81ae1
Achthar <69119535+Achthar@users.noreply.github.com>
2025-01-07 20:38:26 +0100

    upgrade arb composer

--- scripts/arbitrum/addresses/oneDeltaAddresses.ts
+++ scripts/arbitrum/addresses/oneDeltaAddresses.ts
@@ -4,5 +4,5 @@ export const ONE_DELTA_GEN2_ADDRESSES = {
     proxy: "0x816EBC5cb8A5651C902Cb06659907A93E574Db0B",
     managementImplementation: "0x8E24CfC19c6C00c524353CB8816f5f1c2F33c201",
     lensImplementation: "0xcB6Eb8df68153cebF60E1872273Ef52075a5C297",
-    composerImplementation: "0x4b5458BB47dCBC1a41B31b41e1a8773dE312BE9d",
+    composerImplementation: "0x85D682FA4115f6a1Ed91170E705A50D532e3B6BD",
 }
