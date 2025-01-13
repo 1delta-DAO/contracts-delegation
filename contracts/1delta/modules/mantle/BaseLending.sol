ded35050c80bb2041f5a809cfbb326541a34f877
caglavol <volkan@1delta.io>
2025-01-13 16:30:00 +0100

    add erc20 selectors to BaseLending on mantle

--- contracts/1delta/modules/mantle/BaseLending.sol
+++ contracts/1delta/modules/mantle/BaseLending.sol
@@ -18,6 +18,22 @@ abstract contract BaseLending is Slots, ExoticSwapper {
     // errors
     error BadLender();
 
+    ////////////////////////////////////////////////////
+    // ERC20 selectors
+    ////////////////////////////////////////////////////
+
+    /// @dev selector for transferFrom(address,address,uint256)
+    bytes32 internal constant ERC20_TRANSFER_FROM = 0x23b872dd00000000000000000000000000000000000000000000000000000000;
+
+    /// @dev selector for transfer(address,uint256)
+    bytes32 internal constant ERC20_TRANSFER = 0xa9059cbb00000000000000000000000000000000000000000000000000000000;
+
+    /// @dev selector for allowance(address,address)
+    bytes32 internal constant ERC20_ALLOWANCE = 0xdd62ed3e00000000000000000000000000000000000000000000000000000000;
+
+    /// @dev selector for balanceOf(address)
+    bytes32 internal constant ERC20_BALANCE_OF = 0x70a0823100000000000000000000000000000000000000000000000000000000;
+
     // id thresholds
     uint256 internal constant MAX_ID_AAVE_V3 = 1000; // 0-1000
     uint256 internal constant MAX_ID_AAVE_V2 = 2000; // 1000-2000
