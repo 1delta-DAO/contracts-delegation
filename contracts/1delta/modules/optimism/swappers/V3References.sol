d28c12999cd47f27349de4c6899891215a0fe13c
Achthar <69119535+Achthar@users.noreply.github.com>
2025-02-15 16:38:02 +0100

    add op

---
+++ contracts/1delta/modules/optimism/swappers/V3References.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: BUSL-1.1
+
+pragma solidity 0.8.28;
+
+/******************************************************************************\
+* Author: Achthar | 1delta 
+/******************************************************************************/
+
+abstract contract V3ReferencesArbitrum {
+    ////////////////////////////////////////////////////
+    // dex references
+    ////////////////////////////////////////////////////
+
+    bytes32 internal constant UNI_V3_FF_FACTORY = 0xff1F98431c8aD98523631AE4a59f267346ea31F9840000000000000000000000;
+    bytes32 internal constant UNI_POOL_INIT_CODE_HASH = 0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54;
+}
