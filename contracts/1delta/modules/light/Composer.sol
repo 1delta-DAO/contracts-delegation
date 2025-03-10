56f1ee6f0726e189fe28e5016d9b84e201203ba0
Achthar <69119535+Achthar@users.noreply.github.com>
2025-03-10 13:27:51 +0100

    refactor ComposerCommands

--- contracts/1delta/modules/light/Composer.sol
+++ contracts/1delta/modules/light/Composer.sol
@@ -83,20 +83,24 @@ contract OneDeltaComposerLight is
                 // we increment the current offset to skip the operation
                 currentOffset := add(1, currentOffset)
             }
-            if (operation == ComposerCommands.EXT_CALL) {
-                currentOffset = _callExternal(currentOffset);
-            } else if (operation == ComposerCommands.LENDING) {
-                currentOffset = _lendingOperations(callerAddress, paramPull, paramPush, currentOffset);
-            } else if (operation == ComposerCommands.TRANSFERS) {
-                currentOffset = _transfers(currentOffset, callerAddress);
-            } else if (operation == ComposerCommands.PERMIT) {
-                currentOffset = _permit(currentOffset, callerAddress);
-            } else if (operation == ComposerCommands.FLASH_LOAN) {
-                currentOffset = _universalFlashLoan(currentOffset, callerAddress);
-            } else if (operation == ComposerCommands.ERC4646) {
-                currentOffset = _ERC4646Operations(currentOffset, callerAddress);
+            if (operation < ComposerCommands.PERMIT) {
+                if (operation == ComposerCommands.EXT_CALL) {
+                    currentOffset = _callExternal(currentOffset);
+                } else if (operation == ComposerCommands.LENDING) {
+                    currentOffset = _lendingOperations(callerAddress, paramPull, paramPush, currentOffset);
+                } else if (operation == ComposerCommands.TRANSFERS) {
+                    currentOffset = _transfers(currentOffset, callerAddress);
+                }
             } else {
-                _invalidOperation();
+                if (operation == ComposerCommands.PERMIT) {
+                    currentOffset = _permit(currentOffset, callerAddress);
+                } else if (operation == ComposerCommands.FLASH_LOAN) {
+                    currentOffset = _universalFlashLoan(currentOffset, callerAddress);
+                } else if (operation == ComposerCommands.ERC4646) {
+                    currentOffset = _ERC4646Operations(currentOffset, callerAddress);
+                } else {
+                    _invalidOperation();
+                }
             }
             // break criteria - we shifted to the end of the calldata
             if (currentOffset >= maxIndex) break;
