d03c6813acae6d48343450fe6677894901439c2c
Max <0xmaxyz@gmail.com>
2025-03-24 15:55:59 +0330

    refactor the flash loan callback and executer to FlashLoanExecuter

--- contracts/1delta/flash-account/FlashAccount.sol
+++ contracts/1delta/flash-account/FlashAccount.sol
@@ -2,101 +2,18 @@
 pragma solidity ^0.8.27;
 
 import {FlashAccountBase} from "./FlashAccountBase.sol";
+import {FlashLoanExecuter} from "./FlashLoanExecuter.sol";
 import {IEntryPoint} from "account-abstraction/interfaces/IEntryPoint.sol";
-import {ExecutionLock} from "./common/ExecutionLock.sol";
+import {BaseLightAccount} from "./common/BaseLightAccount.sol";
 
-contract FlashAccount is FlashAccountBase, ExecutionLock {
+contract FlashAccount is FlashAccountBase, FlashLoanExecuter {
     constructor(IEntryPoint entryPoint_) FlashAccountBase(entryPoint_) {}
 
-    /// @notice Execute a flash loan
-    /// @param flashLoanProvider The destination address
-    /// @param data The calldata to execute
-    function executeFlashLoan(address flashLoanProvider, bytes calldata data) public onlyAuthorized setInExecution {
-        _call(flashLoanProvider, 0, data);
+    function __call(address target, uint256 value, bytes memory data) internal override {
+        BaseLightAccount._call(target, value, data);
     }
 
-    /**
-     * @dev Explicit flash loan callback functions
-     * All of them are locked through the execution lock to prevent access outside
-     * of the `execute` functions
-     */
-
-    /**
-     * Aave simple flash loan
-     */
-    function executeOperation(
-        address,
-        uint256,
-        uint256,
-        address,
-        bytes calldata params // user params
-    ) external requireInExecution returns (bool) {
-        // forward execution
-        _decodeAndExecute(params);
-
-        return true;
-    }
-
-    /**
-     * Aave V2 flash loan callback
-     */
-    function executeOperation(
-        address[] calldata assets,
-        uint256[] calldata amounts,
-        uint256[] calldata premiums,
-        address initiator,
-        bytes calldata params
-    ) external requireInExecution returns (bool) {
-        // forward execution
-        _decodeAndExecute(params);
-
-        return true;
-    }
-
-    /**
-     * Balancer flash loan
-     */
-    function receiveFlashLoan(
-        address[] calldata,
-        uint256[] calldata,
-        uint256[] calldata,
-        bytes calldata params //
-    ) external requireInExecution {
-        // execute further operations
-        _decodeAndExecute(params);
-    }
-
-    /**
-     * BalancerV3 flash loan
-     */
-    function receiveFlashLoan(bytes calldata data) external requireInExecution {
-        // execute further operations
-        _decodeAndExecute(data);
-    }
-
-    /**
-     * Morpho flash loan
-     */
-    function onMorphoFlashLoan(uint256 assets, bytes calldata params) external requireInExecution {
-        // execute further operations
-        _decodeAndExecute(params);
-    }
-
-    /**
-     * Internal function to decode batch calldata
-     */
-    function _decodeAndExecute(bytes calldata params) internal {
-        (
-            address[] memory dest, //
-            uint256[] memory value,
-            bytes[] memory func
-        ) = abi.decode(params, (address[], uint256[], bytes[]));
-        if (dest.length != func.length || dest.length != value.length) {
-            revert ArrayLengthMismatch();
-        }
-        uint256 length = dest.length;
-        for (uint256 i = 0; i < length; i++) {
-            _call(dest[i], value[i], func[i]);
-        }
+    function __onlyAuthorized() internal view override {
+        BaseLightAccount._onlyAuthorized();
     }
 }
