56c043754fc39c87a88ed999602b6df042cdda05
Achthar <69119535+Achthar@users.noreply.github.com>
2025-04-07 11:15:47 +0200

    a

--- contracts/1delta/modules/light/swappers/callbacks/ValidatorLib.sol
+++ contracts/1delta/modules/light/swappers/callbacks/ValidatorLib.sol
@@ -2,7 +2,7 @@
 
 pragma solidity 0.8.28;
 
-/** This is to check whether  */
+/** This is to check whether the provided parameter is nonempty */
 library ValidatorLib {
     function _hasData(bytes32 data) internal pure returns (bool hasData) {
         assembly {
