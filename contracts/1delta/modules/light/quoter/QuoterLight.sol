1cd886a6caa2e179a2b3e1449ec7f3223b0a80a5
Max <0xmaxyz@gmail.com>
2025-04-07 11:13:56 +0330

    add multihop/multi slice

--- contracts/1delta/modules/light/quoter/QuoterLight.sol
+++ contracts/1delta/modules/light/quoter/QuoterLight.sol
@@ -64,12 +64,13 @@ contract QuoterLight is Masks, V3TypeQuoter, ERC20Selectors {
                 amountIn := mload(0x0)
             }
         }
-        return _quoteSwapSplitOrRoute(amountIn, tokenIn, currentOffset);
+        (uint256 amountOut,,) = _quoteSingleSwapSplitOrRoute(amountIn, tokenIn, currentOffset);
+        return amountOut;
     }
 
-    function _quoteSwapSplitOrRoute(uint256 amountIn, address tokenIn, uint256 currentOffset)
+    function _quoteSingleSwapSplitOrRoute(uint256 amountIn, address tokenIn, uint256 currentOffset)
         internal
-        returns (uint256 amountOut)
+        returns (uint256 amountOut, uint256, address)
     {
         uint256 swapMaxIndex;
         uint256 splitsMaxIndex;
@@ -91,14 +92,17 @@ contract QuoterLight is Masks, V3TypeQuoter, ERC20Selectors {
                 }
 
                 (amountOut, currentOffset) = _quoteSingleSwap(amountIn, tokenIn, nextToken, currentOffset);
+            } else {
+                // Split swap
+                (amountOut, currentOffset, nextToken) =
+                    _quoteSingleSwapOrSplit(amountIn, swapMaxIndex, tokenIn, address(this), currentOffset);
             }
+        } else {
+            // Multi-hop swap
+            (amountOut, currentOffset, nextToken) =
+                _quoteMultiHopSplitSwap(amountIn, swapMaxIndex, tokenIn, currentOffset);
         }
-        // else {
-        //     // Split swap
-        // }
-        // else {
-        //     // Multi-hop swap
-        // }
+        return (amountOut, currentOffset, nextToken);
     }
 
     function _quoteSingleSwap(uint256 amountIn, address tokenIn, address tokenOut, uint256 currentOffset)
@@ -115,5 +119,126 @@ contract QuoterLight is Masks, V3TypeQuoter, ERC20Selectors {
         } else {
             revert InvalidDexId();
         }
+    }
+
+    function _quoteMultiHopSplitSwap(uint256 amountIn, uint256 swapMaxIndex, address tokenIn, uint256 currentOffset)
+        internal
+        returns (uint256, uint256, address)
+    {
+        uint256 amount = amountIn;
+        address _tokenIn = tokenIn;
+        uint256 i;
+        while (true) {
+            (amount, currentOffset, _tokenIn) = _quoteSingleSwapSplitOrRoute(amount, _tokenIn, currentOffset);
+            // loop break condition
+            if (i == swapMaxIndex) {
+                break;
+            } else {
+                i++;
+            }
+        }
+        return (amount, currentOffset, _tokenIn);
+    }
+
+    /**
+     * Ensure that all paths end with the same CCY
+     * parallel swaps a->...->b; a->...->b for different dexs
+     * | Offset | Length (bytes) | Description          |
+     * |--------|----------------|----------------------|
+     * | 0      | 0-16           | splits               |
+     * | sC     | Variable       | datas                |
+     *
+     * `splits` looks like follows
+     * | Offset | Length (bytes) | Description          |
+     * |--------|----------------|----------------------|
+     * | 0      | 1              | count                |
+     * | 1      | 2*count - 1    | splits               | <- count = 0 means there is no data, otherwise uint16 splits
+     *
+     * `datas` looks like follows
+     * | Offset | Length (bytes) | Description          |
+     * |--------|----------------|----------------------|
+     * | 0      | 2              | (r,c)                | <- indicates whether the swap is non-simple (further splits or hops)
+     * | 2      | 1              | dexId                |
+     * | 3      | variable       | params               | <- depends on dexId (fixed for each one)
+     * | 3+v    | 2              | (r,c)                |
+     * | 4+v    | 1              | dexId                |
+     * | ...    | variable       | params               | <- depends on dexId (fixed for each one)
+     * | ...    | ...            | ...                  | <- count + 1 times of repeating this pattern
+     *
+     * returns cumulative output, updated offset and nextToken address
+     */
+    function _quoteSingleSwapOrSplit(
+        uint256 amountIn,
+        uint256 splitsMaxIndex,
+        address tokenIn,
+        address callerAddress, // caller
+        uint256 currentOffset
+    ) internal returns (uint256, uint256, address) {
+        address nextToken;
+        // no splits, single swap
+        if (splitsMaxIndex == 0) {
+            (amountIn, currentOffset, nextToken) = _quoteSingleSwapSplitOrRoute(
+                amountIn,
+                tokenIn, //
+                currentOffset
+            );
+        } else {
+            uint256 splits;
+            assembly {
+                splits := shr(128, calldataload(currentOffset))
+                currentOffset := add(mul(2, splitsMaxIndex), currentOffset)
+            }
+            uint256 amount;
+            uint256 i;
+            uint256 swapsLeft = amountIn;
+            while (true) {
+                uint256 split;
+                assembly {
+                    switch eq(i, splitsMaxIndex)
+                    case 1 {
+                        // assign remaing amount to split
+                        split := swapsLeft
+                    }
+                    default {
+                        // splits are uint16s as share of uint16.max
+                        split :=
+                            div(
+                                mul(
+                                    and(
+                                        UINT16_MASK,
+                                        shr(sub(112, mul(i, 16)), splits) // read the uin16 in the splits sequence
+                                    ),
+                                    amountIn //
+                                ),
+                                UINT16_MASK //
+                            )
+                    }
+                    i := add(i, 1)
+                }
+                uint256 received;
+                // reenter-universal swap
+                // can be another split or a multi-path
+                (received, currentOffset, nextToken) = _quoteSingleSwapSplitOrRoute(
+                    split,
+                    tokenIn, //
+                    currentOffset
+                );
+
+                // increment and decrement
+                assembly {
+                    amount := add(amount, received)
+                }
+
+                // if nothing is left, break
+                if (i > splitsMaxIndex) break;
+
+                // otherwise, we decrement the swaps left amount
+                assembly {
+                    swapsLeft := sub(swapsLeft, split)
+                }
+            }
+            amountIn = amount;
+        }
+        return (amountIn, currentOffset, nextToken);
     }
 }
