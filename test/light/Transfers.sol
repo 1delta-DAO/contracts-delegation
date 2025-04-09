510a64fdcfa1ce2c3720fde644441763950e6e55
Max <0xmaxyz@gmail.com>
2025-04-09 08:08:02 +0330

    add some tests for transfer and sweep

--- test/light/Transfers.sol
+++ test/light/Transfers.sol
@@ -11,8 +11,9 @@ import {IERC20All} from "../shared/interfaces/IERC20All.sol";
 import {BaseTest} from "../shared/BaseTest.sol";
 import {Chains, Tokens, Lenders} from "../data/LenderRegistry.sol";
 import "./utils/CalldataLib.sol";
+import {DeltaErrors} from "modules/shared/errors/Errors.sol";
 
-contract TransfersLightTest is BaseTest {
+contract TransfersLightTest is BaseTest, DeltaErrors {
     using MorphoMathLib for uint256;
 
     OneDeltaComposerLight oneD;
@@ -42,7 +43,9 @@ contract TransfersLightTest is BaseTest {
 
     uint256 internal constant UPPER_BIT = 1 << 255;
 
-    /** Sweep amount in contract */
+    /**
+     * Sweep amount in contract
+     */
     function test_light_sweep_token_amount() external {
         address asset = WETH;
         uint256 sweepAm = 30.0e18;
@@ -61,5 +64,108 @@ contract TransfersLightTest is BaseTest {
         uint256 balanceAfter = IERC20All(asset).balanceOf(user);
 
         assertApproxEqAbs(balanceAfter - balanceBefore, sweepAm, 0, "Sweep failed");
+    }
+
+    function test_light_sweep_validate() external {
+        uint256 initialAmount = 1000e6;
+        uint256 minBalance = 500e6;
+
+        deal(USDC, address(oneD), initialAmount);
+
+        bytes memory data = CalldataLib.sweep(USDC, user, minBalance, CalldataLib.SweepType.VALIDATE);
+
+        vm.prank(user);
+        oneD.deltaCompose(data);
+
+        assertEq(IERC20All(USDC).balanceOf(user), initialAmount);
+        assertEq(IERC20All(USDC).balanceOf(address(oneD)), 0);
+    }
+
+    function test_light_sweep_validate_reverts() external {
+        uint256 initialAmount = 499e6;
+        uint256 minBalance = 500e6;
+
+        deal(USDC, address(oneD), initialAmount);
+
+        bytes memory data = CalldataLib.sweep(USDC, user, minBalance, CalldataLib.SweepType.VALIDATE);
+
+        vm.prank(user);
+        vm.expectRevert(SLIPPAGE);
+        oneD.deltaCompose(data);
+
+        assertEq(IERC20All(USDC).balanceOf(user), 0);
+        assertEq(IERC20All(USDC).balanceOf(address(oneD)), initialAmount);
+    }
+
+    function test_light_sweep_balance() external {
+        uint256 initialAmount = 1000e6;
+        deal(USDC, address(oneD), initialAmount);
+
+        bytes memory data = CalldataLib.sweep(
+            USDC,
+            user,
+            0, // minBalance
+            CalldataLib.SweepType.VALIDATE
+        );
+
+        vm.prank(user);
+        oneD.deltaCompose(data);
+
+        assertEq(IERC20All(USDC).balanceOf(user), initialAmount);
+        assertEq(IERC20All(USDC).balanceOf(address(oneD)), 0);
+    }
+
+    function test_light_sweep_native() external {
+        uint256 initialAmount = 1 ether;
+        vm.deal(address(oneD), initialAmount);
+        uint256 userInitialBalance = user.balance;
+
+        // native sweep
+        bytes memory data = CalldataLib.sweep(
+            address(0),
+            user,
+            0, // min balance => all balance (for validate mode)
+            CalldataLib.SweepType.VALIDATE
+        );
+
+        vm.prank(user);
+        oneD.deltaCompose(data);
+
+        assertEq(user.balance, userInitialBalance + initialAmount);
+        //assertEq(address(oneD).balance, 0);
+    }
+
+    function test_light_transfer_transferIn_zero() external {
+        // zero means entire balance
+        uint256 initialAmount = 1000e6;
+        deal(USDC, user, initialAmount);
+
+        vm.prank(user);
+        IERC20All(USDC).approve(address(oneD), type(uint256).max);
+
+        bytes memory data = CalldataLib.transferIn(USDC, address(oneD), 0);
+
+        vm.prank(user);
+        oneD.deltaCompose(data);
+
+        assertEq(IERC20All(USDC).balanceOf(user), 0);
+        assertEq(IERC20All(USDC).balanceOf(address(oneD)), initialAmount);
+    }
+
+    function test_light_transfer_transferIn_specific() external {
+        uint256 initialAmount = 1000e6;
+        uint256 transferAmount = 500e6;
+        deal(USDC, user, initialAmount);
+
+        vm.prank(user);
+        IERC20All(USDC).approve(address(oneD), type(uint256).max);
+
+        bytes memory data = CalldataLib.transferIn(USDC, address(oneD), transferAmount);
+
+        vm.prank(user);
+        oneD.deltaCompose(data);
+
+        assertEq(IERC20All(USDC).balanceOf(user), initialAmount - transferAmount);
+        assertEq(IERC20All(USDC).balanceOf(address(oneD)), transferAmount);
     }
 }
