c32d4a345b1fed726babfd1c7607b5e777497654
Max <0xmaxyz@gmail.com>
2025-05-09 10:24:35 +0330

    convert to assembly

--- contracts/1delta/composer/generic/bridges/StargateV2/StargateV2.sol
+++ contracts/1delta/composer/generic/bridges/StargateV2/StargateV2.sol
@@ -1,9 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {BaseUtils} from "contracts/1delta/composer/generic/BaseUtils.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./IStargate.sol";
 
 contract StargateV2 is BaseUtils {
@@ -13,159 +11,116 @@ contract StargateV2 is BaseUtils {
      * @param currentOffset Current position in the calldata
      * @return Updated calldata offset after processing
      *
-     * | Offset | Length (bytes) | Description                  |
-     * |--------|----------------|------------------------------|
-     * | 0      | 20             | tokenAddress                 |
-     * | 20     | 2              | assetId                      |
-     * | 22     | 20             | stargate pool                |
-     * | 42     | 4              | dstEid                       |
-     * | 46     | 32             | receiver                     |
-     * | 78     | 20             | refundReceiver               |
-     * | 98     | 16             | amount                       |
-     * | 114    | 4              | slippage                     |
-     * | 118    | 16             | fee                          |
-     * | 134    | 1              | isBusMode                    |
-     * | 135    | 2              | composeMsg.length: cl        |
-     * | 137    | 2              | extraOptions.length: el      |
-     * | 139    | cl             | composeMsg: cm               |
-     * | 139+cl | el             | extraOptions: eo             |
+     * | Offset       | Length (bytes) | Description                  |
+     * |--------------|----------------|------------------------------|
+     * | 0            | 20             | tokenAddress                 |
+     * | 20           | 20             | stargate pool                |
+     * | 40           | 4              | dstEid                       |
+     * | 44           | 32             | receiver                     |
+     * | 76           | 20             | refundReceiver               |
+     * | 96           | 16             | amount                       |
+     * | 112          | 4              | slippage                     |
+     * | 116          | 16             | fee                          |
+     * | 132          | 1              | isBusMode                    |
+     * | 133          | 2              | composeMsg.length: cl        |
+     * | 135          | 2              | extraOptions.length: el      |
+     * | 137          | cl             | composeMsg                   |
+     * | 137+cl       | el             | extraOptions                 |
      */
     function _bridgeStargateV2(uint256 currentOffset) internal returns (uint256) {
         BridgeParams memory params;
-        uint16 composeMsgLength;
-        uint16 extraOptionsLength;
-        address asset;
+
         assembly {
-            asset := shr(96, calldataload(currentOffset))
-            // Load assetId (2 bytes)
-            mstore(params, and(shr(240, calldataload(add(currentOffset, 20))), UINT16_MASK))
-
-            // Load stargate pool (20 bytes)
-            mstore(add(params, 32), shr(96, calldataload(add(currentOffset, 22))))
-
-            // Load dstEid (4 bytes)
-            mstore(add(params, 64), and(shr(224, calldataload(add(currentOffset, 42))), UINT32_MASK))
-
-            // Load receiver (32 bytes)
-            mstore(add(params, 96), calldataload(add(currentOffset, 46)))
-
-            // Load refundReceiver (20 bytes)
-            mstore(add(params, 256), shr(96, calldataload(add(currentOffset, 78))))
-
-            // Load amount (16 bytes)
-            mstore(add(params, 128), shr(128, calldataload(add(currentOffset, 98))))
-
-            // Load slippage (4 bytes)
-            mstore(add(params, 160), and(shr(224, calldataload(add(currentOffset, 114))), UINT32_MASK))
-
-            // Load fee (16 bytes)
-            mstore(add(params, 192), shr(128, calldataload(add(currentOffset, 118))))
-
-            // Load isBusMode (1 byte)
-            mstore(add(params, 224), and(shr(248, calldataload(add(currentOffset, 134))), UINT8_MASK))
-
-            // Load lengths
-            composeMsgLength := and(shr(240, calldataload(add(currentOffset, 135))), UINT16_MASK)
-            extraOptionsLength := and(shr(240, calldataload(add(currentOffset, 137))), UINT16_MASK)
-        }
-
-        if (composeMsgLength > 0) {
-            params.composeMsg = new bytes(composeMsgLength);
-            assembly {
-                calldatacopy(
-                    add(mload(add(params, 288)), 0x20), // Pointer to composeMsg data area
-                    add(currentOffset, 139),
-                    composeMsgLength
-                )
+            function revertWith(code) {
+                mstore(0, code)
+                revert(0, 4)
             }
-        }
 
-        if (extraOptionsLength > 0) {
-            params.extraOptions = new bytes(extraOptionsLength);
-            assembly {
-                calldatacopy(
-                    add(mload(add(params, 320)), 0x20), // Pointer to extraOptions data area
-                    add(add(currentOffset, 139), composeMsgLength),
-                    extraOptionsLength
-                )
+            function getBalance(token) -> b {
+                mstore(0, ERC20_BALANCE_OF)
+                mstore(0x04, address())
+                pop(staticcall(gas(), token, 0x0, 0x24, 0x0, 0x20))
+                b := mload(0x0)
             }
+
+            let asset
+            let composeMsgLength
+            let extraOptionsLength
+            let oftCmdLength
+            let amount
+            let requiredValue
+            let fee
+
+            asset := shr(96, calldataload(add(currentOffset)))
+            fee := shr(128, calldataload(add(currentOffset, 116)))
+
+            composeMsgLength := and(shr(240, calldataload(add(currentOffset, 133))), UINT16_MASK)
+            extraOptionsLength := and(shr(240, calldataload(add(currentOffset, 135))), UINT16_MASK)
+            amount := and(shr(128, calldataload(add(currentOffset, 60))), UINT128_MASK)
+            let isNative := and(NATIVE_FLAG, amount)
+            // clear the native flag
+            amount := and(amount, not(NATIVE_FLAG))
+
+            // check if amount is 0
+            switch iszero(amount)
+            case 1 {
+                switch isNative
+                case 1 {
+                    // amount is the balance minus the fee
+                    amount := sub(selfbalance(), fee)
+                    // and value to send is everything
+                    requiredValue := selfbalance()
+                }
+                default {
+                    // use token balance
+                    amount := getBalance(asset)
+                    // value to send is just the fee
+                    requiredValue := fee
+                    // check if fee is enough
+                    if gt(requiredValue, selfbalance()) { revertWith(INSUFFICIENT_VALUE) }
+                }
+            }
+            default {
+                switch isNative
+                case 1 {
+                    // value to send is amount desired plus fee
+                    requiredValue := add(amount, fee)
+                }
+                default {
+                    // erc20 case: value is just the fee
+                    requiredValue := fee
+                }
+                // check if we have enough to pay the fee
+                if gt(requiredValue, selfbalance()) { revertWith(INSUFFICIENT_VALUE) }
+            }
+
+            let minAmountLD :=
+                div(mul(amount, sub(FEE_DENOMINATOR, and(shr(224, calldataload(add(currentOffset, 112))), UINT32_MASK))), FEE_DENOMINATOR)
+            let compose
+
+            let ptr := mload(0x40)
+            mstore(ptr, 0xcbef2aa900000000000000000000000000000000000000000000000000000000) // sendToken selector
+            mstore(add(ptr, 0x04), and(shr(224, calldataload(add(currentOffset, 40))), UINT32_MASK)) // dstEid
+            mstore(add(ptr, 0x24), calldataload(add(currentOffset, 44))) // to
+            mstore(add(ptr, 0x44), amount) // amountLD
+            mstore(add(ptr, 0x64), minAmountLD) // minAmountLD
+            mstore(add(ptr, 0x84), 0x160) // extraOptions offset
+            mstore(add(ptr, 0xa4), add(0x160, add(extraOptionsLength, 0x20))) // composeMsg offset
+            mstore(add(ptr, 0xc4), add(add(0x160, add(extraOptionsLength, 0x20)), add(composeMsgLength, 0x20))) // oftCmd offset
+            mstore(add(ptr, 0xe4), fee) // nativeFee
+            mstore(add(ptr, 0x104), 0) // lzFee
+            mstore(add(ptr, 0x124), shr(96, calldataload(add(currentOffset, 76)))) // refundAddress
+            mstore(add(ptr, 0x144), extraOptionsLength) // extraOptions length
+            calldatacopy(add(ptr, 0x164), add(currentOffset, 137), extraOptionsLength) // extraOptions
+            mstore(add(add(ptr, 0x164), extraOptionsLength), composeMsgLength) // composeMsg length
+            calldatacopy(add(add(ptr, 0x184), extraOptionsLength), add(add(currentOffset, 137), extraOptionsLength), composeMsgLength) // composeMsg
+            mstore(add(add(add(ptr, 0x184), extraOptionsLength), composeMsgLength), 1) // oftCmd length
+            mstore(add(add(add(ptr, 0x1a4), extraOptionsLength), composeMsgLength), and(shr(248, calldataload(add(currentOffset, 132))), UINT8_MASK)) // is bus mode
+            // call stargate
+
+            if iszero(call(gas(), shr(96, calldataload(add(currentOffset, 20))), requiredValue, ptr, 0x1c4, 0, xxxx)) { revert(0, xxxx) }
         }
 
-        _bridgeTokens(asset, params);
-
-        // Calculate new offset
         return currentOffset + 139 + composeMsgLength + extraOptionsLength;
-    }
-
-    function _bridgeTokens(address tokenAddr, BridgeParams memory _params) internal {
-        // Check if the token is native or ERC20
-        bool isNative = tokenAddr == address(0);
-
-        // get the native balance at the beginning
-        uint256 selfNativeBalance;
-        assembly {
-            selfNativeBalance := selfbalance()
-        }
-
-        // initialize the total callvalue to send
-        uint256 requiredValue;
-
-        // if amount is 0, then use the balance of the contract
-        if (_params.amount == 0) {
-            if (isNative) {
-                // amount is the balance minus the fee
-                _params.amount = selfNativeBalance - _params.fee;
-                // and value to send is everything
-                requiredValue = selfNativeBalance;
-            } else {
-                // use token balance
-                _params.amount = IERC20(tokenAddr).balanceOf(address(this));
-                // value to send is just the fee
-                requiredValue = _params.fee;
-                // check if fee is enough
-                if (requiredValue > selfNativeBalance) revert InsufficientValue();
-            }
-        } else {
-            if (isNative) {
-                // value to send is amount desired plus fee
-                requiredValue = _params.amount + _params.fee;
-            } else {
-                // erc20 case: value is just the fee
-                requiredValue = _params.fee;
-            }
-            // check if we have enough to pay the fee
-            if (requiredValue > selfNativeBalance) revert InsufficientValue();
-        }
-
-        // Create the sendParam structure
-        IStargate.SendParam memory sendParam = IStargate.SendParam({
-            dstEid: uint32(_params.dstEid),
-            to: _params.receiver,
-            amountLD: _params.amount,
-            // use standard slippage adjustment for slippage
-            minAmountLD: (_params.amount * (FEE_DENOMINATOR - _params.slippage)) / FEE_DENOMINATOR,
-            extraOptions: _params.extraOptions,
-            composeMsg: _params.composeMsg,
-            oftCmd: _params.isBusMode ? new bytes(1) : new bytes(0) // Bus or taxi mode
-        });
-
-        // call stargate
-        (bool success, bytes memory data) = _params.stargatePool.call{value: requiredValue}(
-            abi.encodeWithSelector(
-                IStargate.sendToken.selector,
-                sendParam,
-                IStargate.MessagingFee({nativeFee: _params.fee, lzTokenFee: 0}),
-                // We need this as a custom parameter
-                // that is because the caller can be another composer
-                payable(_params.refundAddress)
-            )
-        );
-
-        // forward the error if any
-        if (!success) {
-            assembly {
-                revert(add(data, 32), mload(data))
-            }
-        }
     }
 }
