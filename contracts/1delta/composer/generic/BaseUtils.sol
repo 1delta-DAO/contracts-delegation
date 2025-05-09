c2ea332d273e27e1d722290366ff01b3ee1c0f2e
Max <0xmaxyz@gmail.com>
2025-05-09 10:25:01 +0330

    add byte4 selectors for errors

--- contracts/1delta/composer/generic/BaseUtils.sol
+++ contracts/1delta/composer/generic/BaseUtils.sol
@@ -7,10 +7,13 @@ import {DeltaErrors} from "contracts/1delta/shared/errors/Errors.sol";
 
 contract BaseUtils is ERC20Selectors, Masks, DeltaErrors {
     error InvalidAssetId(uint16 assetId);
-    error InsufficientValue(); //0x1101129400000000000000000000000000000000000000000000000000000000
+    error InsufficientValue();
     error SlippageTooHigh(uint256 expected, uint256 actual);
-    error ZeroBalance(); // 0x669567ea00000000000000000000000000000000000000000000000000000000
-    error BridgeFailed(); // 0xc3b9eede00000000000000000000000000000000000000000000000000000000
+    error ZeroBalance();
+    error BridgeFailed();
 
     uint256 internal constant FEE_DENOMINATOR = 1e9;
+    uint256 internal constant INSUFFICIENT_VALUE = 0x1101129400000000000000000000000000000000000000000000000000000000;
+    uint256 internal constant ZERO_BALANCE = 0x669567ea00000000000000000000000000000000000000000000000000000000;
+    uint256 internal constant BRIDGE_FAILED = 0xc3b9eede00000000000000000000000000000000000000000000000000000000;
 }
