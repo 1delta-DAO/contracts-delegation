af962116ae8eb56e4cba17420b8d757337837b37
Achthar <69119535+Achthar@users.noreply.github.com>
2025-05-16 13:14:07 +0200

    refactor

--- contracts/1delta/composer/generic/ExternalCallsGeneric.sol
+++ contracts/1delta/composer/generic/ExternalCallsGeneric.sol
@@ -17,21 +17,14 @@ abstract contract ExternalCallsGeneric is BaseUtils {
     bytes4 private constant FORBIDDEN = 0xee90c468;
 
     function _callExternal(uint256 currentOffset) internal returns (uint256) {
-        ////////////////////////////////////////////////////
-        // Execute call to external contract. It consits of
-        // an approval target and call target.
-        // The combo of [approvalTarget, target] has to be whitelisted
-        // for calls. Those are exclusively swap aggregator contracts.
-        // An amount has to be supplied to check the allowance from
-        // this contract to target.
-        // NEVER whitelist a token as an attacker can call
-        // `transferFrom` on target
-        // Data layout:
-        //      bytes 0-20:                  target
-        //      bytes 20-34:                 callvalue()
-        //      bytes 34-36:                 calldataLength
-        //      bytes 36-(36+data length):   data
-        ////////////////////////////////////////////////////
+        /*
+         * | Offset | Length (bytes) | Description          |
+         * |--------|----------------|----------------------|
+         * | 0      | 20             | target               |
+         * | 20     | 14             | nativeValue          |
+         * | 34     | 2              | calldataLength:  cl  |
+         * | 36     | cl             | calldata             |
+         */
         assembly {
             // get first three addresses
             let target := shr(96, calldataload(currentOffset))
@@ -83,4 +76,111 @@ abstract contract ExternalCallsGeneric is BaseUtils {
         }
         return currentOffset;
     }
+
+    function _tryCallExternal(uint256 currentOffset, address callerAddress) internal returns (uint256) {
+        /*
+         * | Offset | Length (bytes) | Description          |
+         * |--------|----------------|----------------------|
+         * | 0      | 20             | target               |
+         * | 20     | 14             | nativeValue          |
+         * | 34     | 2              | calldataLength:  cl  |
+         * | 36     | cl             | calldata             |
+         * | 36+cl  | 1              | catchHandling        | <- 0: revert, 1:
+         * | 37+cl  | 2              | catchDataLength: dl  |
+         * | 39+cl  | dl             | catchData            |
+         */
+        bool success;
+        uint256 catchHandling;
+        uint256 catchCalldataLength;
+        assembly {
+            let target := shr(96, calldataload(currentOffset))
+
+            // prevent calls to permit2
+            if eq(target, 0x000000000022D473030F116dDEE9F6B43aC78BA3) {
+                mstore(0x0, FORBIDDEN)
+                revert(0x0, 0x4)
+            }
+
+            currentOffset := add(20, currentOffset)
+            // get msg.value for call
+            let callValue := calldataload(currentOffset)
+            let dataLength := and(UINT16_MASK, shr(128, callValue))
+            callValue := shr(144, callValue) // shr will already mask correctly
+
+            if iszero(callValue) { callValue := selfbalance() }
+
+            // free memo ptr for populating the tx
+            let ptr := mload(0x40)
+
+            // increment offset to calldata start
+            currentOffset := add(16, currentOffset)
+
+            // extract the selector from the calldata
+            // and check if it is `transferFrom`
+            if eq(and(SELECTOR_MASK, calldataload(currentOffset)), ERC20_TRANSFER_FROM) {
+                mstore(0x0, FORBIDDEN)
+                revert(0x0, 0x4)
+            }
+
+            // copy calldata
+            calldatacopy(ptr, currentOffset, dataLength)
+            success :=
+                call(
+                    gas(),
+                    target,
+                    callValue,
+                    ptr, //
+                    dataLength,
+                    // the length must be correct or the call will fail
+                    0x0, // output = empty
+                    0x0 // output size = zero
+                )
+
+            // increment offset by data length
+            currentOffset := add(currentOffset, dataLength)
+
+            let nexstSlice := calldataload(currentOffset)
+            // top byte of next slice
+            catchHandling := shr(248, nexstSlice)
+            // case: 0 revert if not successful
+            if iszero(catchHandling) {
+                if iszero(success) {
+                    returndatacopy(0, 0, returndatasize())
+                    revert(0, returndatasize())
+                }
+            }
+
+            catchCalldataLength := and(UINT16_MASK, shr(236, nexstSlice))
+            currentOffset := add(currentOffset, 3)
+        }
+
+        // execution logic on failute
+        if (!success) {
+            // Catch and run fallback
+            // execute further calldata if provided
+            if (catchCalldataLength > 0) {
+                // if calldata is provided, compose the remaining data
+                _deltaComposeInternal(callerAddress, currentOffset, catchCalldataLength);
+            }
+            // case 1 - exit funciton execution here
+            if (catchHandling == 1) {
+                assembly {
+                    return(0, 0)
+                }
+            }
+        }
+        // increment offset by additional data length
+        assembly {
+            currentOffset := add(currentOffset, catchCalldataLength)
+        }
+        return currentOffset;
+    }
+
+    function _deltaComposeInternal(
+        address callerAddress,
+        uint256 currentOffset,
+        uint256 calldataLength //
+    )
+        internal
+        virtual;
 }
