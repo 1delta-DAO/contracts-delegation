58df0a0f2d1f1a249c9d688646d51dd7b09e8fb5
Achthar <69119535+Achthar@users.noreply.github.com>
2025-10-11 22:33:38 +0200

    [forwarder] amend script

--- scripts/_shared/deployForwarderTest.ts
+++ scripts/_shared/deployForwarderTest.ts
@@ -6,14 +6,14 @@ import {keccak256} from "ethers/lib/utils";
 /**
 export FACTORY="0x16c4Dc0f662E2bEceC91fC5E7aeeC6a25684698A"
 export CALLER="0x16c4Dc0f662E2bEceC91fC5E7aeeC6a25684698A"
-export INIT_CODE_HASH="0xc2c9ab00fa915409071ea6dc4064f5dbf00112843be430bfb721e57f4c7e3414"
+export INIT_CODE_HASH="0xef99898e103157bbb5524b3caac121c5030dbb37a94e432a3f2fa84c7da5ffb9"
 export GPU_DEVICE=255
 export ADDRESS_START_WITH="fca11"
-export ADDRESS_END_WITH="03"
+export ADDRESS_END_WITH="04"
 cargo run --release $FACTORY $CALLER $INIT_CODE_HASH $GPU_DEVICE $ADDRESS_START_WITH $ADDRESS_END_WITH
  */
 
-// 0x16c4dc0f662e2becec91fc5e7aeec6a25684698a9c651296615ea600372a78a5 => 0xfCa11d8c816f8E52F55D5d9858dEEe78F152d903 (1 / 0)
+// 0x16c4dc0f662e2becec91fc5e7aeec6a25684698ad8a8c12ea9f7f4038e82e82e => 0xfCa11d0E353C5c91D62AA5feC05cE39af9393204 (1 / 0)
 
 async function main() {
     const accounts = await ethers.getSigners();
@@ -27,10 +27,10 @@ async function main() {
 
     console.log("Get deploy factory");
 
-    const salt = "0x16c4dc0f662e2becec91fc5e7aeec6a25684698a9c651296615ea600372a78a5";
-    //  deploys to 0xfCa11d8c816f8E52F55D5d9858dEEe78F152d903
+    const salt = "0x16c4dc0f662e2becec91fc5e7aeec6a25684698ad8a8c12ea9f7f4038e82e82e";
+    //  deploys to 0xfCa11d0E353C5c91D62AA5feC05cE39af9393204
     const bytecode = CallForwarder__factory.bytecode;
-
+    console.log("bytecode", bytecode);
     const initCode = keccak256(bytecode);
     console.log("initCode", initCode); // 0x6d95759de5f9c1ff23720012281168c1b9cdc928be6790a9eb2efdc32bad0980 (paris, 1m steps)
 
@@ -39,8 +39,8 @@ async function main() {
     console.log("address", address);
     const estimate = await deployFactory.estimateGas.deploy(salt, bytecode);
     console.log("estimate", estimate);
-    const tx = await deployFactory.deploy(salt, bytecode, {gasLimit: estimate.add(100), gasPrice: gp});
-    await tx.wait();
+    // const tx = await deployFactory.deploy(salt, bytecode, {gasLimit: estimate.add(100), gasPrice: gp});
+    // await tx.wait();
 
     console.log("deployed expected to", address);
 }
