de692f407f4604464549ced358f31ca247e50867
Max <0xmaxyz@gmail.com>
2025-10-21 20:52:05 +0330

    add repay and withdraw

--- test/composer/lending/compound/debased_compoundV2.sol
+++ test/composer/lending/compound/debased_compoundV2.sol
@@ -15,8 +15,8 @@ contract CompoundV2ComposerLightTest is BaseTest {
 
     IComposerLike oneDV2;
 
-    address internal USDC;
-    address internal USDC_CTOKEN;
+    address internal WETH;
+    address internal WETH_CTOKEN;
 
     address internal USDT;
     address internal USDT_CTOKEN;
@@ -27,62 +27,55 @@ contract CompoundV2ComposerLightTest is BaseTest {
     uint256 internal constant forkBlock = 290934482;
 
     function setUp() public virtual {
-        // initialize the chain
         string memory chainName = Chains.ARBITRUM_ONE;
 
         _init(chainName, forkBlock, true);
         lender = Lenders.VENUS;
-        USDC = chain.getTokenAddress(Tokens.USDC);
-        USDC_CTOKEN = _getCollateralToken(USDC);
+        WETH = chain.getTokenAddress(Tokens.WETH);
+        WETH_CTOKEN = _getCollateralToken(WETH);
         USDT = chain.getTokenAddress(Tokens.USDT);
         USDT_CTOKEN = _getCollateralToken(USDT);
         VENUS_COMPTROLLER = chain.getLendingController(lender);
 
         oneDV2 = ComposerPlugin.getComposer(chainName);
 
-        vm.label(USDC, "USDC");
+        vm.label(WETH, "WETH");
         vm.label(USDT, "USDT");
         vm.label(VENUS_COMPTROLLER, "VENUS");
         vm.label(address(oneDV2), "oneDV2");
         vm.label(user, "user");
-        vm.label(USDC_CTOKEN, "USDC_CTOKEN");
+        vm.label(WETH_CTOKEN, "WETH_CTOKEN");
         vm.label(USDT_CTOKEN, "USDT_CTOKEN");
     }
 
     function test_debased() external {
-        uint256 amount = 1e12; // 1 million
-        deal(USDC, user, amount);
+        uint256 amount = 100 ether;
+        deal(WETH, user, amount);
 
         address[] memory cTokens = new address[](1);
-        cTokens[0] = USDC_CTOKEN;
+        cTokens[0] = WETH_CTOKEN;
 
         vm.prank(user);
         IERC20All(VENUS_COMPTROLLER).enterMarkets(cTokens);
 
         vm.prank(user);
-        IERC20All(USDC).approve(address(oneDV2), amount);
+        IERC20All(WETH).approve(address(oneDV2), amount);
 
-        bytes memory depositCallData = CalldataLib.encodeTransferIn(USDC, address(oneDV2), amount);
+        bytes memory depositCallData = CalldataLib.encodeTransferIn(WETH, address(oneDV2), amount);
 
         depositCallData = abi.encodePacked(
-            depositCallData, CalldataLib.encodeCompoundV2Deposit(USDC, amount, user, USDC_CTOKEN, uint8(CompoundV2Selector.MINT_BEHALF))
+            depositCallData, CalldataLib.encodeCompoundV2Deposit(WETH, amount, user, WETH_CTOKEN, uint8(CompoundV2Selector.MINT_BEHALF))
         );
 
         vm.prank(user);
         oneDV2.deltaCompose(depositCallData);
 
-        approveBorrowDelegation(user, USDC, address(oneDV2), lender);
+        approveBorrowDelegation(user, WETH, address(oneDV2), lender);
 
         uint256 amountToBorrow = 100000e6;
         bytes memory borrowCallData = CalldataLib.encodeCompoundV2Borrow(USDT, amountToBorrow, user, USDT_CTOKEN);
         vm.prank(user);
         oneDV2.deltaCompose(borrowCallData);
-
-        // uint256 amountToWithdraw = 100000.0e6;
-        // bytes memory d = CalldataLib.encodeCompoundV2Withdraw(USDT, amountToWithdraw, user, USDT_CTOKEN, uint8(CompoundV2Selector.REDEEM_BEHALF));
-
-        // vm.prank(user);
-        // oneDV2.deltaCompose(d);
 
         // uint256 bb = 0;
         // (bool success, bytes memory data) = USDT_CTOKEN.call(abi.encodeWithSelector(0x17bfdfbc, user));
@@ -90,6 +83,29 @@ contract CompoundV2ComposerLightTest is BaseTest {
         //     bb = abi.decode(data, (uint256));
         // }
         // console.log("bb", bb);
+
+        vm.prank(user);
+        IERC20All(USDT).approve(address(oneDV2), type(uint256).max);
+        bytes memory transferTo = CalldataLib.encodeTransferIn(
+            USDT,
+            address(oneDV2),
+            amountToBorrow //
+        );
+
+        bytes memory d = CalldataLib.encodeCompoundV2Repay(USDT, type(uint112).max, user, USDT_CTOKEN);
+        vm.prank(user);
+        oneDV2.deltaCompose(abi.encodePacked(transferTo, d));
+
+        vm.prank(user);
+        IERC20All(WETH_CTOKEN).approve(address(oneDV2), type(uint256).max);
+
+        vm.prank(user);
+        oneDV2.deltaCompose(
+            CalldataLib.encodeCompoundV2Withdraw(WETH, type(uint112).max, address(oneDV2), WETH_CTOKEN, uint8(CompoundV2Selector.REDEEM))
+        );
+
+        uint256 bb = IERC20All(WETH).balanceOf(address(oneDV2));
+        console.log("Difference", amount - bb);
     }
 
     function _getCollateralToken(address token) internal view returns (address) {
