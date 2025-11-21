2f216fbc87a430cce84076f0f109476595d2ff5d
Max <0xmaxyz@gmail.com>
2025-11-21 15:48:49 +0330

    add nft receiver to call forwarder

--- contracts/1delta/composer/generic/CallForwarder.sol
+++ contracts/1delta/composer/generic/CallForwarder.sol
@@ -6,6 +6,7 @@ import {ComposerCommands} from "../enums/DeltaEnums.sol";
 import {Transfers} from "../transfers/Transfers.sol";
 import {ExternalCallsGeneric} from "../generic/ExternalCallsGeneric.sol";
 import {BridgeForwarder} from "./bridges/BridgeForwarder.sol";
+import {ERC721Receiver} from "./ERC721Receiver.sol";
 
 /**
  * @notice An arbitrary call contract to forward generic calls
@@ -15,7 +16,7 @@ import {BridgeForwarder} from "./bridges/BridgeForwarder.sol";
  * Can generically call any target and checks if the selector for these calls is not `transferFrom`
  * We assume that this contract is never an approve target!
  */
-contract CallForwarder is Transfers, ExternalCallsGeneric, BridgeForwarder {
+contract CallForwarder is Transfers, ExternalCallsGeneric, BridgeForwarder, ERC721Receiver {
     // base receive function
     receive() external payable {}
 
